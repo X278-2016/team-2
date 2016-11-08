cf9e5b261e38d6d6a7434ef98181950df8ef70c0
Brennan Belew <brennan.c.belew@vanderbilt.edu>
2016-11-08 12:15:43 -0600

    new login screen code

--- TestProject/index.android.js
+++ TestProject/index.android.js
@@ -1,102 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
+'use strict';
+import { Component } from 'react';
+import React, {
   AppRegistry,
-  Image,
-  StyleSheet,
   Text,
-  TextInput,
-  View
+  View,
+  Navigator,
+  AsyncStorage
 } from 'react-native';
 
-export default class TestProject extends Component {
-  constructor(props) {
-      super(props);
-      this.state = { 
-          email: 'Email',
-          confirmEmail: 'Confirm',
-          password: '',
-          confirmPassword: ''
-      };
-  }
-  render() {
-    let pic = {
-      uri: 'https://image.freepik.com/free-icon/thumbs-up_318-31579.jpg'
+import Signup from './src/pages/signup';
+import Account from './src/pages/account';
+
+import Header from './src/components/header';
+
+import Firebase from 'firebase';
+
+let app = new Firebase("YOUR-FIREBASE-APP-URL");
+
+import styles from './src/styles/common-styles.js';
+
+class TestProject extends Component {    
+
+  constructor(props){
+    super(props);
+    this.state = {
+      component: null,
+      loaded: false
     };
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Sign up below!
-        </Text>
-        <Text style={styles.instructions}>
-          Please fill in all required fields
-        </Text>
-        <Text style={styles.instructions}>
-          Email*
-        </Text>
-        <TextInput 
-          style={{flex:0.1, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
-          selectTextOnFocus={true}
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-        />
-        <Text style={styles.instructions}>
-          Confirm Email*
-        </Text>
-        <TextInput 
-          style={{flex: 0.1, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
-          selectTextOnFocus={true}
-          onChangeText={(confirmEmail) => this.setState({confirmEmail})}
-          value={this.state.confirmEmail}
-        />
-        <Text style={styles.instructions}>
-          Password*
-        </Text>
-        <TextInput 
-          style={{flex: 0.1, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
-          selectTextOnFocus={true}
-          secureTextEntry={true}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-        />
-        <Text style={styles.instructions}>
-          Confirm Password*
-        </Text>
-        <TextInput 
-          style={{flex: 0.1, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
-          selectTextOnFocus={true}
-          secureTextEntry={true}
-          onChangeText={(confirmPassword) => this.setState({confirmPassword})}
-          value={this.state.confirmPassword}
-        />
-        <Image source={pic} style={{width: 200, height: 200}}/>
-      </View>
-    );
   }
+
+  componentWillMount(){
+
+    AsyncStorage.getItem('user_data').then((user_data_json) => {
+
+      let user_data = JSON.parse(user_data_json);
+      let component = {component: Signup};
+      if(user_data != null){
+        app.authWithCustomToken(user_data.token, (error, authData) => {
+          if(error){
+            this.setState(component);
+          }else{
+            this.setState({component: Account});
+          }
+        });
+      }else{
+        this.setState(component);
+      }
+    });
+
+  }
+
+  render(){
+
+    if(this.state.component){
+      return (
+        <Navigator
+          initialRoute={{component: this.state.component}}
+          configureScene={() => {
+            return Navigator.SceneConfigs.FloatFromRight;
+          }}
+          renderScene={(route, navigator) => {
+            if(route.component){
+              return React.createElement(route.component, { navigator });
+            }
+          }}
+        />
+      );
+    }else{
+      return (
+        <View style={styles.container}>
+          <Header text="React Native Firebase Auth" loaded={this.state.loaded} />  
+          <View style={styles.body}></View>
+        </View>
+      );
+    }
+
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 0.4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TestProject', () => TestProject);
