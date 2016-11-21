e80ff5daba71c3765cf93f1d825b5de329a54004
Zach Barton <zachary.m.barton@vanderbilt.edu>
2016-11-20 20:27:02 -0600

    First draft of formatted signup page

--- MBB/Signup.ios.js
+++ MBB/Signup.ios.js
@@ -14,9 +14,10 @@ import SignupSuccess from './SignupSuccess.ios.js';
 export default class Signup extends Component {
 constructor(props) {
       super(props);
-      this.state = { 
-          email: 'Email',
-          confirmEmail: 'Confirm',
+      this.state = {
+          name: '',
+          email: '',
+          confirmEmail: '',
           password: '',
           confirmPassword: ''
       };
@@ -27,59 +28,64 @@ constructor(props) {
     };
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text style={styles.title}>
           Sign up below!
         </Text>
         <Text style={styles.instructions}>
-          Please fill in all required fields
+          Required fields are marked with an asterisk*
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={styles.label}>
+          Name*
+        </Text>
+        <TextInput 
+          style={styles.input}
+          selectTextOnFocus={true}
+          onChangeText={(name) => this.setState({name})}
+          value={this.state.name}
+        />
+        <Text style={styles.label}>
           Email*
         </Text>
         <TextInput 
-          style={{height: 25, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
+          style={styles.input}
           selectTextOnFocus={true}
           onChangeText={(email) => this.setState({email})}
           value={this.state.email}
         />
-        <Text style={styles.instructions}>
+        <Text style={styles.label}>
           Confirm Email*
         </Text>
         <TextInput 
-          style={{height: 25, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
+          style={styles.input}
           selectTextOnFocus={true}
           onChangeText={(confirmEmail) => this.setState({confirmEmail})}
           value={this.state.confirmEmail}
         />
-        <Text style={styles.instructions}>
+        <Text style={styles.label}>
           Password*
         </Text>
         <TextInput 
-          style={{height: 25, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
+          style={styles.input}
           selectTextOnFocus={true}
           secureTextEntry={true}
           onChangeText={(password) => this.setState({password})}
           value={this.state.password}
         />
-        <Text style={styles.instructions}>
+        <Text style={styles.label}>
           Confirm Password*
         </Text>
         <TextInput 
-          style={{height: 25, textAlign: 'center', color: 'darkgray', borderColor: 'black'}}
+          style={styles.input}
           selectTextOnFocus={true}
           secureTextEntry={true}
           onChangeText={(confirmPassword) => this.setState({confirmPassword})}
           value={this.state.confirmPassword}
         />
         
-        <TouchableHighlight onPress={this.onSignup.bind(this)}>
-          <Text> Submit! </Text>
+        <TouchableHighlight style={styles.button} onPress={this.onSignup.bind(this)}>
+          <Text style={styles.buttonText}> Submit </Text>
         </TouchableHighlight>
-        
-        <TouchableHighlight onPress={this.onBack.bind(this)}>
-          <Text>Tap me to go back</Text>
-        </TouchableHighlight>
-        <Image source={pic} style={{width: 200, height: 200}}/>
+          {/*<Image source={pic} style={{width: 200, height: 200}}/> */}
       </View>
     );
   }
@@ -97,18 +103,49 @@ constructor(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 50,
+    //backgroundColor: 'darkblue',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    backgroundColor: 'white',
   },
   instructions: {
     textAlign: 'center',
+    marginBottom: 25,
+  },
+  label: {
+    textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    backgroundColor: 'white',
   },
+  input: {
+    borderStyle: 'solid',
+    backgroundColor: 'whitesmoke',
+    height: 25,
+    marginLeft: 25,
+    marginRight: 25,
+    marginBottom: 10,
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  button: {
+    borderStyle: 'solid',
+    borderColor: 'darkblue',
+    backgroundColor: 'red',
+    height: 50,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
