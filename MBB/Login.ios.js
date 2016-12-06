d794e65e0a798c4e08340b562cf4add63941daaa
Zach Barton <zachary.m.barton@vanderbilt.edu>
2016-12-05 19:23:45 -0600

    Saving login changes?

--- MBB/Login.ios.js
+++ MBB/Login.ios.js
@@ -3,16 +3,19 @@ import {
   Image,
   StyleSheet,
   Text,
+  Alert,
   TextInput,
   TouchableHighlight,
   View
 } from 'react-native';
 
 
-import Home from './Home.ios.js';
-import Signup from './Signup.ios.js';
-import pwReset from './PasswordReset.ios.js';
-import Profile from './Profile.ios.js';
+import Home from './Home.android.js';
+import Signup from './Signup.android.js';
+import pwReset from './PasswordReset.android.js';
+import Profile from './Profile.android.js';
+import logo from './logo-draft1-copy3-constrained.png';
+import background from './background-gradient-final.png';
 
 import * as firebase from 'firebase';
 const firebaseconfig = {
@@ -34,64 +37,96 @@ export default class Login extends Component {
   }
   render() {
     return (
-      <View style={styles.layout}>
-        <Text style={styles.title}>
-          Login
-        </Text>
-      {/*<Text style={styles.label}>
-          You are signed in as {firebase.auth().currentUser.uid}
-        </Text>*/}
-        <Text style={styles.label}>
-          Email
-        </Text>
-        <TextInput
-          style={styles.input}
-          selectTextOnFocus={true}
-          onChangeText={(email) => this.setState({email})}
-          value={this.state.email}
-        />
-        <Text style={styles.label}>
-          Password
-        </Text>
-        <TextInput 
-          style={styles.input}
-          selectTextOnFocus={true}
-          secureTextEntry={true}
-          onChangeText={(password) => this.setState({password})}
-          value={this.state.password}
-        />
-        <TouchableHighlight style={styles.loginButton}
-        onPress={this.onLogin.bind(this)}>
-          <Text style={styles.loginText}>
-            Enter
+      <Image 
+        style={styles.backgroundImage}
+        source = {background}>
+          <View style = {styles.layout}>
+          <View style = {{flexDirection: 'row'}}>
+            <Image 
+              style={styles.imageContainer}
+              resizeMode='contain'
+              source = {logo}
+            />
+          </View>
+          <Text style={styles.title}>
+            Login
           </Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={styles.helpButton}
-        onPress={this.pwReset.bind(this)}>
-          <Text style={styles.helpText}>
-            Forgot your password?
+          <Text style={styles.label}>
+            Email
           </Text>
-        </TouchableHighlight>
-        <TouchableHighlight style={styles.helpButton}
-        onPress={this.onSignup.bind(this)}>
-          <Text style={styles.helpText}>
-            New to Hub? Sign up here!
+          <View style = {{flexDirection: 'row'}}>
+              <View style={{flex:0.15}}></View>
+              <TextInput
+                style={styles.input}
+                selectTextOnFocus={true}
+                onChangeText={(email) => this.setState({email})}
+                value={this.state.email}
+              />
+              <View style={{flex:0.15}}></View>
+          </View>
+          <Text style={styles.label}>
+            Password
           </Text>
-        </TouchableHighlight>
-      </View>
+          <View style = {{flexDirection: 'row'}}>
+              <View style={{flex:0.15}}></View>
+              <TextInput 
+                style={styles.input}
+                selectTextOnFocus={true}
+                secureTextEntry={true}
+                onChangeText={(password) => this.setState({password})}
+                value={this.state.password}
+              />
+              <View style={{flex:0.15}}></View>
+          </View>
+          <TouchableHighlight style={styles.loginButton}
+          onPress={this.onLogin.bind(this)}>
+            <Text style={styles.loginText}>
+              Enter
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.helpButton}
+          onPress={this.pwReset.bind(this)}>
+            <Text style={styles.helpText}>
+              Forgot your password?
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.helpButton}
+          onPress={this.onSignup.bind(this)}>
+            <Text style={styles.helpText}>
+              New to Hub? Sign up here!
+            </Text>
+          </TouchableHighlight>
+        </View>  
+      </Image>
     );
   }
   
   onLogin(){
-   firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error){
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error){
       var errorCode = error.code;
       var errorMessage = error.message;
       //Handle errors and stuff here
+      switch (errorCode) {
+        case 'auth/user-disabled':
+          Alert.alert('Account Disabled','The account corresponding to the email you entered has been disabled.');
+          break;
+        case 'auth/invalid-email':
+          Alert.alert('Invalid Email','The email address you entered is not a valid email.');
+          break;
+        case 'auth/user-not-found':
+          Alert.alert('User Not Found','There is no account with the email you entered.');
+          break;
+        case 'auth/wrong-password':
+          Alert.alert('Wrong Password','The email and password you entered do not match.');
+          break;
+      }
     });
     firebase.auth().onAuthStateChanged(function(user) {
-      this.props.navigator.push({
-        component: Home
-      });
+      if (user) {
+        this.props.navigator.push({
+          component: Profile
+        });
+      }
     }.bind(this));
   }
   
@@ -113,55 +148,73 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'darkblue',
   },
   title: {
-    fontSize: 40,
+    fontFamily: 'sans-serif',
+    fontSize: 28,
     fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  label: {
+    color: 'firebrick',
     textAlign: 'center',
     marginBottom: 5,
-    color: 'white',
+  },
+  label: {
+    fontFamily: 'sans-serif',
+    textAlign: 'center',
+    marginBottom: 5,
+    color: 'firebrick',
   },
   input: {
-    borderStyle: 'solid',
-    backgroundColor: 'whitesmoke',
-    height: 25,
-    marginLeft: 25,
-    marginRight: 25,
-    marginBottom: 10,
+    flex:0.7,
+    height: 35,
+    marginBottom: 5,
     alignItems: 'center',
     textAlign: 'center',
+    backgroundColor: 'whitesmoke',
+    borderStyle: 'solid',
+    borderColor: '#87cefa',
+    borderWidth: 1,
   },
   loginButton: {
-    borderStyle: 'solid',
-    borderColor: 'darkblue',
-    backgroundColor: 'red',
-    height: 50,
+    height: 40,
     width: 250,
-    marginBottom: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
   loginText: {
-    color: 'white',
-    fontSize: 30,
+    fontFamily: 'sans-serif',
+    color: 'firebrick',
+    fontSize: 28,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   helpButton: {
     height: 30,
     width: 200,
     alignItems: 'center',
-    justifyContent:'center',
-    marginBottom: 20,
+    marginBottom: 5,
+    marginTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
   helpText: {
-    color: 'white',
+    fontFamily: 'sans-serif',
+    color: 'midnightblue',
     fontSize: 15,
     textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+  imageContainer: {
+    flexShrink: 1,
+    height: 180,
+    //alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    alignItems:'center',
+    justifyContent:'center',
   }
 });
